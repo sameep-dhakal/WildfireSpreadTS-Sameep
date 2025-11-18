b59ca1d0f7d098186749fe0819107e9feb73ee57
sameep-dhakal <sameep.dhakal721108@gmail.com>
2025-11-18 17:35:18 -0600

    smp model changed

--- src/models/SMPModel.py
+++ src/models/SMPModel.py
@@ -37,6 +37,58 @@
 
 
 # new code for encoder weights
+# from typing import Any
+
+# import segmentation_models_pytorch as smp
+
+# from .BaseModel import BaseModel
+
+
+# class SMPModel(BaseModel):
+#     """_summary_ Segmentation model based on the SMP package. We only use the U-Net model. 
+#     """
+#     def __init__(
+#         self,
+#         encoder_name: str = "resnet18",
+#         encoder_weights: str = "imagenet",
+#         n_channels: int = 7,
+#         flatten_temporal_dimension: bool = True,
+#         pos_class_weight: float = 1.0,
+#         loss_function: str = "Focal",
+#         use_doy: bool = False,
+#         crop_before_eval: bool = False,
+#         required_img_size=None,
+#         alpha_focal=None,
+#         f1_threshold=None,
+#         **kwargs
+#     ):
+#         # ✅ Explicitly call BaseModel.__init__ with matching params
+#         super().__init__(
+#             n_channels=n_channels,
+#             flatten_temporal_dimension=flatten_temporal_dimension,
+#             pos_class_weight=pos_class_weight,
+#             loss_function=loss_function,
+#             use_doy=use_doy,
+#             crop_before_eval=crop_before_eval,
+#             required_img_size=required_img_size,
+#             alpha_focal=alpha_focal,
+#             f1_threshold=f1_threshold,
+#             **kwargs
+#         )
+
+#         self.save_hyperparameters()
+#         encoder_weights = encoder_weights if encoder_weights != "none" else None
+#         self.model = smp.Unet(
+#             encoder_name=encoder_name,  # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+#             encoder_weights=encoder_weights,  # use `imagenet` pre-trained weights for encoder initialization
+#             in_channels=n_channels,  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+#             classes=1,  # model output channels (number of classes in your dataset)
+#         )
+#         print(f"Loaded {encoder_name} with {encoder_weights} weights")
+
+        
+
+# new code for encoder weights
 from typing import Any
 
 import segmentation_models_pytorch as smp
@@ -72,6 +124,4 @@ class SMPModel(BaseModel):
             in_channels=n_channels,  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
             classes=1,  # model output channels (number of classes in your dataset)
         )
-        print(f"Loaded {encoder_name} with {encoder_weights} weights")
-
-        
+        print(f"Loaded {encoder_name} with {encoder_weights} weights")     
