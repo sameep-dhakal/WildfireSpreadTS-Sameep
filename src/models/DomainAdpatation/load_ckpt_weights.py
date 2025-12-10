197e9244e6822c4d949094ee3a695b2894c8ee15
sameep-dhakal <sameep.dhakal721108@gmail.com>
2025-12-09 20:10:50 -0600

    cehck stage 2 results

--- src/models/DomainAdpatation/load_ckpt_weights.py
+++ src/models/DomainAdpatation/load_ckpt_weights.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader
 
 from models.SMPModel import SMPModel
 from dataloader.FireSpreadDataset import FireSpreadDataset
-from models.DomainAdpatation.IWANStage2_WeightEstimator import DomainheadCNN
+from models.DomainAdpatation.IWANStage2_WeightEstimator import DomainHead3x1024
 
 
 
